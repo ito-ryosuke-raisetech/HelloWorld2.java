e98a3f57022efbca41a6f8fbb68f3c13cbc95bcc
ito-ryosuke-raisetech <85591560+ito-ryosuke-raisetech@users.noreply.github.com>
2021-06-14 07:44:27 +0900

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,11 @@
+package HelloWorld2;
+
+public class HelloWorld2 {
+//　メインプログラム
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      String greeting = "Hello";
+      String name="World";
+      System.out.println(greeting + name );
+  }
+}
